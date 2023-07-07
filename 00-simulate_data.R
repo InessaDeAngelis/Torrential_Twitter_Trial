af7daab17147bc22420804127bea125fa4f406ea
Inessa De Angelis <131813572+InessaDeAngelis@users.noreply.github.com>
2023-07-07 11:34:42 -0400

    Add files via upload

---
+++ 00-simulate_data.R
@@ -0,0 +1,17 @@
+#### Preamble ####
+# Purpose: Simulates... [...UPDATE THIS...]
+# Author: Inessa De Angelis
+# Date: 12 June 2023
+# Contact: inessa.deangelis@mail.utoronto.ca
+# License: MIT
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Simulate data ####
+# [...ADD CODE HERE...]
+
+
+
