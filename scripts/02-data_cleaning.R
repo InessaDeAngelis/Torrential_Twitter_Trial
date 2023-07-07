73da6b35f88e65deed22a7e11dc26411c4fb4f77
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-07-07 15:57:05 -0400

    Wrote script to select columns of interest and filter Tweets by key words related to the environment and climate change

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -20,3 +20,67 @@ readr::read_csv("~/Desktop/Old Twitter Data/ElizabethMayMentions.csv")
 
 # Read in the Laurel Collins data set #
 readr::read_csv("~/Desktop/Old Twitter Data/LaurelMentions.csv")
+
+#### Clean Catherine McKenna data set ####
+raw_mckenna_data <-
+  read_csv(
+    file = "~/Desktop/Old Twitter Data/CathMckenna.csv",
+    show_col_types = FALSE
+  )
+
+# Select columns of interest & filter by key words #
+# Code referenced from: https://www.statology.org/filter-rows-that-contain-string-dplyr/#:~:text=Often%20you%20may%20want%20to,()%20function%20in%20Base%20R.
+cleaned_mckenna_data = 
+  raw_mckenna_data |>
+  select(
+    text,
+    user_sname,
+    user_description,
+    user_location,
+    followers,
+    friends,
+    tweet_url
+  ) |>
+  filter(grepl('environment | climate', text))
+
+#### Clean Elizabeth May data set ####
+raw_may_data <-
+  read_csv(
+    file = "~/Desktop/Old Twitter Data/ElizabethMayMentions.csv",
+    show_col_types = FALSE
+  )
+
+# Select columns of interest & filter by key words #
+cleaned_may_data = 
+  raw_may_data |>
+  select(
+    text,
+    user_sname,
+    user_description,
+    user_location,
+    followers,
+    friends,
+    tweet_url
+  ) |>
+  filter(grepl('environment | climate', text))
+
+#### Clean Laurel Collins data set ####
+raw_collins_data <-
+  read_csv(
+    file = "~/Desktop/Old Twitter Data/LaurelMentions.csv",
+    show_col_types = FALSE
+  )
+
+# Select columns of interest & filter by key words #
+cleaned_collins_data = 
+  raw_collins_data |>
+  select(
+    text,
+    user_sname,
+    user_description,
+    user_location,
+    followers,
+    friends,
+    tweet_url
+  ) |>
+  filter(grepl('environment | climate | pipeline', text))
