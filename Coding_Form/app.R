e397e579a8e1f48a71e2f3216a3849d5cadc367b
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-07-10 15:30:34 -0400

    Updated code to create coding form

--- Coding_Form/app.R
+++ Coding_Form/app.R
@@ -2,50 +2,34 @@
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
+# Code referenced from: https://deanattali.com/2015/06/14/mimicking-google-form-shiny/ 
 #
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application that codes data
+shinyApp(
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+    titlePanel("Torrential Twitter Data Coding Form"),
+    div(
+      id = "form",
+      textInput("coder_name", " Coder Name"),
+      selectInput("pol_name", "Name of Politician",
+                  c("Catherine McKenna", "Elizabeth May", "Laurel Collins")),
+      textInput("tweet_url", "Tweet URL"),
+      textInput("tweet_text", "Tweet Text"),
+      textInput("username", "Username"),
+      textInput("name", "Name"),
+      textInput("bio", "Bio"),
+      selectInput("severity", "Severity of Harassment",
+                  c("1", "2", "3", "4", "5", "6", "7")),
+      selectInput("account_type", "Type of Account",
+                  c("1", "2", "3", "4", "5")),
+      actionButton("submit", "Submit", class = "btn-primary")
     )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+    ),
+server = function(input, output, session) {
 }
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+)
