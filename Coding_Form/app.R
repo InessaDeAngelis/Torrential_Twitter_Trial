5352d9a3511269ab068c734fd9ce511c0e61a5f0
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-07-12 14:47:43 -0400

    Updates to preamble, app description, and minor changes to the form categories

--- Coding_Form/app.R
+++ Coding_Form/app.R
@@ -1,17 +1,24 @@
+#### Preamble ####
+# Purpose: Creates a form with Shiny to code Tweets for severity of harassment and type of account 
+# Author: Inessa De Angelis
+# Date: 9 July 2023
+# Contact: inessa.deangelis@mail.utoronto.ca
+# License: MIT
+# Prerequisites: 01-raw_data_cleaning.R (to ensure your data is ready to code via this form)
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Code referenced from: https://deanattali.com/2015/06/14/mimicking-google-form-shiny/ 
-#
+# This is a Shiny web application. You can run the application by clicking the 'Run App' button above.
 
+#### Workspace set up ####
 library(shiny)
+library(DT)
 
+#### App design ####
+# Code referenced from: https://deanattali.com/2015/06/14/mimicking-google-form-shiny/ 
 # Define UI for application that codes data
 shinyApp(
 ui <- fluidPage(
 
-    # Application title
+    # Application title & form categories 
     titlePanel("Torrential Twitter Data Coding Form"),
     div(
       id = "form",
@@ -26,7 +33,7 @@ ui <- fluidPage(
       selectInput("severity", "Severity of Harassment",
                   c("1", "2", "3", "4", "5", "6", "7")),
       selectInput("account_type", "Type of Account",
-                  c("1", "2", "3", "4", "5")),
+                  c("1", "2", "3", "4", "5", "6")),
       actionButton("submit", "Submit", class = "btn-primary")
     )
     ),
