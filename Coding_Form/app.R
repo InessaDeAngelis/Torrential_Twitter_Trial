06e55a5759773cc404335e62442f460bdb5f3490
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-07-13 10:44:12 -0400

    Updates

--- Coding_Form/app.R
+++ Coding_Form/app.R
@@ -5,12 +5,13 @@
 # Contact: inessa.deangelis@mail.utoronto.ca
 # License: MIT
 # Prerequisites: 01-raw_data_cleaning.R (to ensure your data is ready to code via this form)
-#
+# Corequisite: CodeBook.pdf 
+
 # This is a Shiny web application. You can run the application by clicking the 'Run App' button above.
 
 #### Workspace set up ####
 library(shiny)
-library(DT)
+library(here)
 
 #### App design ####
 # Code referenced from: https://deanattali.com/2015/06/14/mimicking-google-form-shiny/ 
@@ -37,6 +38,32 @@ ui <- fluidPage(
       actionButton("submit", "Submit", class = "btn-primary")
     )
     ),
-server = function(input, output, session) {
+  server <- function(input, output, session) {
 }
 )
+
+# Save form outputs as a CSV file
+outputDir <- (here::here("~/inessadeangelis/torrential_twitter_trial/coding_form"))
+
+saveData <- function(data) {
+  data <- t(data)
+  # Create a unique file name
+  fileName <- sprintf("%s_%s.csv", as.integer(Sys.time()), digest::digest(data))
+  # Write the file to the local system
+  write.csv(
+    x = data,
+    file = file.path(outputDir, fileName), 
+    row.names = FALSE, quote = TRUE
+  )
+}
+loadData <- function() {
+  # Read all the files into a list
+  files <- list.files(outputDir, full.names = TRUE)
+  data <- lapply(files, read.csv, stringsAsFactors = FALSE) 
+  # Concatenate all data together into one data.frame
+  data <- do.call(rbind, data)
+  data
+}
+
+#Run the app
+shinyApp(ui, server)
