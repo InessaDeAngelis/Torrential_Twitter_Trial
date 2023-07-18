f75b2836bf3cd2f9ca3b3e96f2bb40ffb32cca29
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-07-18 09:51:30 -0400

    Decided to split the testing of the first and second rounds of data cleaning into two seperate scripts. Set up document, workspace, read in cleaned data, and began to write tests using "validate"

---
+++ scripts/04-test_finalized_data.R
@@ -0,0 +1,46 @@
+#### Preamble ####
+# Purpose: Tests the finalized data sets
+# Author: Inessa De Angelis
+# Date: 18 July 2023
+# Contact: inessa.deangelis@mail.utoronto.ca 
+# License: MIT
+# Pre-requisites
+# 02-coded_data_cleaning.R
+
+#### Workspace setup ####
+library(tidyverse)
+library(testthat)
+library(validate)
+
+#### Read in the coded cleaned data sets ####
+# Read in the cleaned McKenna data # 
+cleaned_mckenna_data <- read.csv(here::here("~/Desktop/Final Paper/Outputs/Data/finalized_mckenna_data.csv"))
+show_col_types = FALSE
+
+# Read in the cleaned May data #
+cleaned_may_data <- read.csv(here::here("~/Desktop/Final Paper/Outputs/Data/finalized_may_data.csv"))
+
+# Read in the cleaned Collins data #
+cleaned_collins_data <- read.csv(here::here("~/Desktop/Final Paper/Outputs/Data/finalized_collins_data.csv"))
+
+#### Check the coded and cleaned McKenna data set ####
+# Code referenced from: https://tellingstorieswithdata.com/09-clean_and_prepare.html#checking-and-testing #
+rules <- validator(
+  is.character(tweet_text),
+  is.character(username),
+  is.character(name),
+  is.character(bio),
+  is.character(severity_of_harassment),
+  is.character(type_of_account),
+  severity_of_harassment %in% c("Positive", "Neutral", "Questioning Authority", "Name-calling/Gender insults", "Vicious language", "Credible threats", "Hate speech")
+)
+
+out <-
+  confront(finalized_mckenna_data, rules)
+
+summary(out)
+
+#### Check the coded and cleaned May data set ####
+
+
+#### Check the coded and cleaned Collins data set ####
